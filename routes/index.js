996653c0f91f0dd8c6614ccd7af695549aa76814
David_Yu <yuyang545262477@gmail.com>
2016-04-13 16:52:41 +0800

    完善路由规则部分

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,34 @@
  * */
 
 function routes(app) {
+    //首页
     app.get('/', function (req, res) {
-        res.render('index', {title: '主页.'});
+        res.render('index', {title: '主页'});
     });
+    //注册页面
     app.get('/reg', function (req, res) {
         res.render('reg', {title: '注册页面'})
     });
+    app.poster('/reg', function (req, res) {
+
+    });
+    //文章发表页面
     app.get('/post', function (req, res) {
         res.render('post', {title: '文章发表页面'})
     });
+    app.post('/post', function (req, res) {
+
+    });
+    //登录页面
     app.get('/login', function (req, res) {
         res.render('login', {title: '登录页面'})
+    });
+    app.post('login', function (req, res) {
+
+    });
+//    登出页面
+    app.get('/logout', function (req, res) {
+
     });
 
 }
