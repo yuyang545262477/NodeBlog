914a13da7420057a2ef1067976a150abd828f51a
David_Yu <yuyang545262477@gmail.com>
2016-04-13 16:45:58 +0800

    增加App的路由规则. '/',主页 '/reg',注册 '/login',登录 '/post',文章发布

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,21 @@
-module.exports = function (app) {
-    app.get('/',function (req, res) {
-        res.render('index',{title:'主页'});
-    })
-};
+/*
+ * 这里的型参,假设就是express的实例,事实上,也是.
+ * */
+
+function routes(app) {
+    app.get('/', function (req, res) {
+        res.render('index', {title: '主页.'});
+    });
+    app.get('/reg', function (req, res) {
+        res.render('reg', {title: '注册页面'})
+    });
+    app.get('/post', function (req, res) {
+        res.render('post', {title: '文章发表页面'})
+    });
+    app.get('/login', function (req, res) {
+        res.render('login', {title: '登录页面'})
+    });
+
+}
+
+module.exports = routes;
