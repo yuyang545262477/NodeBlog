22c507a293afd7ed681c6558d158389e7e82de4b
yuyang545262477 <yuyang545262477@gmail.com>
2016-04-15 01:19:27 +0800

    在linux上测试

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
+app.use(flash());
 
 //设置中间件
 app.use(session({
