f6645fce6e2a86ddf2fb146c7f4a276844a37fab
David_Yu <yuyang545262477@gmail.com>
2016-04-15 16:40:26 +0800

    清空已完成项目. 准备第三次重写.

--- models/db.js
+++
@@ -1,7 +0,0 @@
-var setting = require('../settings'),
-    Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = require('mongodb').Server;
-
-module.exports = new Db(setting.db, new Server(setting.host, setting.port), {safe: true});
-    
