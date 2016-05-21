157be5c83af0d8b97a9607826f6de2ad09cde7c3
David_Yu <yuyang545262477@gmail.com>
2016-05-21 14:03:48 +0800

    更新路由。

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index', {title: 'hello world '});
+    });
+    app.get('/yuyang', function (req, res) {
+        res.render('yuyang', {title: 'yuyang'});
+    })
+};
